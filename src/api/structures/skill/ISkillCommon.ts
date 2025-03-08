c7d1024ee9f549303cdf279f956ef98d1c16aeeb
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-08 02:44:16 +0000

    feat: ILink Type

---
+++ src/api/structures/skill/ISkillCommon.ts
@@ -0,0 +1,31 @@
+import { tags } from "typia";
+
+export namespace ISkillCommon {
+  /**
+   * Skill을 위한 Link
+   *
+   * `ISkillCommon.ILink`은 PC, Modile, Web 환경에 대한 외부 Link 정보를 담습니다.
+   *
+   * Link 적용 우선순위:
+   * - PC 환경: pc < web
+   * - Mobile 환경: mobile < web
+   *
+   * 위 규칙에 따라 항상 web이 먼저 동작합니다.
+   */
+  export interface ILink {
+    /**
+     * PC 환경에서 작동할 url link
+     */
+    pc: (string & tags.Format<"url">) | null;
+
+    /**
+     * Mobile 환경에서 작동할 url link
+     */
+    mobile: (string & tags.Format<"url">) | null;
+
+    /**
+     * Web 환경에서 작동할 url link
+     */
+    web: (string & tags.Format<"url">) | null;
+  }
+}
