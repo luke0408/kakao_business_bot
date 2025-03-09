6afc822f8e2c32d14a163c39375dfb1ce70ab0e7
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-09 07:21:46 +0000

    feat: create button layout type

--- src/api/structures/skill/ISkillCommon.ts
+++ src/api/structures/skill/ISkillCommon.ts
@@ -78,6 +78,16 @@ export namespace ISkillCommon {
     | IButtonAction.IBlockButton
     | IButtonAction.IShareButton
     | IButtonAction.IOperatorButton;
+
+  /**
+   * Button Layout 정보를 정의합니다.
+   *
+   * - Vertical: 최대 3개의 버튼을 세로로 배치
+   * - Horizontal: 최대 2개의 버튼을 가로로 배치
+   */
+  export interface IButtonLayout {
+    buttonLayout: "vertical" | "horizontal";
+  }
 }
 
 export namespace IButtonAction {
