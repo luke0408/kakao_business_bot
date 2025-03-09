e1f042eae16b76aa6c3a62a6cc320934e4519eb9
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-09 06:55:39 +0000

    feat: IPhoneNumber 패턴 국제 번호 규격으로 수정

--- src/api/structures/skill/ISkillCommon.ts
+++ src/api/structures/skill/ISkillCommon.ts
@@ -78,11 +78,24 @@ export namespace ISkillCommon {
 }
 
 export namespace IButtonAction {
+  /**
+   * PhoneNumber의 pettern을 정의한 validation type
+   *
+   * 국제 번호 규격으로 구현되어있습니다.
+   *
+   * @example
+   *  ```ts
+   *  const a: IPhoneNumber = "+1 (123) 456-7890";  // pass
+   *  const b: IPhoneNumber = "+82 10-1234-5678";   // pass
+   *  const c: IPhoneNumber = "010-1234-5678";      // pass
+   *  const d: IPhoneNumber = "abcdefg";           // error
+   *  ```
+   */
   export type IPhoneNumber = tags.TagBase<{
     kind: "phoneNumber";
     target: "string";
     value: undefined;
-    validate: `/^\+?[1-9]\d{1,14}$/.test($input)`;
+    validate: `/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test($input)`;
   }>;
 
   /**
