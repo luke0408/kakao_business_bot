b88fd78216b8cb265117ffd4df2ba52fae644c1a
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-10 06:00:49 +0000

    refactor: ILink 분리

--- src/api/structures/skill/template/ISkillCommon.ts
+++ src/api/structures/skill/template/ISkillCommon.ts
@@ -1,42 +1,14 @@
 import { tags } from "typia";
 
+import { ILink } from "./ILink";
+
 export namespace ISkillCommon {
-  /**
-   * Skill Link
-   *
-   * `ISkillCommon.ILink`은 PC, Mobile, Web 환경에 대한 외부 Link 정보를 담습니다.
-   *
-   * Link 적용 우선순위:
-   * - PC 환경: pc < web
-   * - Mobile 환경: mobile < web
-   *
-   * 위 규칙에 따라 항상 web이 먼저 동작합니다.
-   */
-  export interface ILink {
-    /**
-     * PC 환경에서 작동할 url link
-     */
-    pc: (string & tags.Format<"url">) | null;
-
-    /**
-     * Mobile 환경에서 작동할 url link
-     */
-    mobile: (string & tags.Format<"url">) | null;
-
-    /**
-     * Web 환경에서 작동할 url link
-     */
-    web: (string & tags.Format<"url">) | null;
-  }
-
   /**
    * Skill Thumbnail Image
    *
    * `ISkillCommon.IThumbnail`은 thumbnail image에 대한 정보를 담습니다.
    */
-  export interface IThumbnail<
-    Link extends ISkillCommon.ILink = ISkillCommon.ILink,
-  > {
+  export interface IThumbnail<Link extends ILink = ILink> {
     /**
      * Thumbnail Image url
      */
@@ -48,7 +20,7 @@ export namespace ISkillCommon {
     altText: (string & tags.MaxLength<50>) | null;
 
     /**
-     * 이미지 클릭시 동작하는 {@link ISkillCommon.ILink}
+     * 이미지 클릭시 동작하는 {@link ILink}
      */
     link: Link | null;
 
