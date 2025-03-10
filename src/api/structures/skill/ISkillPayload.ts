e72c0a890b0a7edf6ab72eea0f7a3ca0f1206fec
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-10 06:35:50 +0000

    feat: knowledge type

--- src/api/structures/skill/ISkillPayload.ts
+++ src/api/structures/skill/ISkillPayload.ts
@@ -1,3 +1,5 @@
+import { tags } from "typia";
+
 export namespace ISkillPayload {
   /**
    * 사용자의 발화 단위를 정하는 block 정보
@@ -5,5 +7,18 @@ export namespace ISkillPayload {
   export interface IBlock {
     id: string;
     name: string;
+  }
+
+  export namespace IIntent {
+    /**
+     * 지식 정보
+     */
+    export interface IKnowledge {
+      answer: string;
+      question: string;
+      categories: string;
+      langingUrl: string & tags.Format<"url">;
+      imageUrl: string & tags.Format<"url">;
+    }
   }
 }
