07750e2cc5e84fabe5bd85f1189b0eacea0e0d6e
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-24 03:29:48 +0000

    feat: simple image type

--- src/api/structures/skill/template/IOutputs.ts
+++ src/api/structures/skill/template/IOutputs.ts
@@ -8,4 +8,9 @@ export namespace IOutputs {
   export interface ISimpleText {
     text: string & tags.MaxLength<1000>;
   }
+
+  export interface ISimpleImage {
+    imageUrl: string & tags.Format<"uri">;
+    altText: string & tags.MaxLength<50>;
+  }
 }
