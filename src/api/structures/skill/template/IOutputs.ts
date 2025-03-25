b5c23340789cc849370ba8cda086376ca3c95cd5
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-25 09:59:10 +0000

    feat: text card

--- src/api/structures/skill/template/IOutputs.ts
+++ src/api/structures/skill/template/IOutputs.ts
@@ -1,5 +1,7 @@
 import { tags } from "typia";
 
+import { IButton } from "./IButton";
+
 export type IOutput = IOutputs.ISimpleText | IOutputs.ISimpleImage;
 
 export type IOutputs = IOutput[] & tags.MaxItems<3> & tags.MinItems<1>;
@@ -12,5 +14,57 @@ export namespace IOutputs {
   export interface ISimpleImage {
     imageUrl: string & tags.Format<"uri">;
     altText: string & tags.MaxLength<50>;
+  }
+
+  export type ITextCard = ITextCard.ITitledCard | ITextCard.IDescripedCard;
+
+  /**
+   * Namespace containing interfaces for text card structures.
+   */
+  export namespace ITextCard {
+    /**
+     * Base interface for a card.
+     */
+    export interface ICardBase {
+      /**
+       * The title of the card. It can be a string with a maximum length of 50 characters or null.
+       */
+      title: (string & tags.MaxLength<50>) | null;
+
+      /**
+       * The description of the card. It can be a string with a maximum length of 400 characters or null.
+       */
+      description: (string & tags.MaxLength<400>) | null;
+
+      /**
+       * An array of buttons associated with the card. The array must have at least 1 item and at most 3 items.
+       */
+      buttons: IButton[] & tags.MaxItems<3> & tags.MinItems<1>;
+
+      /**
+       * The layout of the buttons on the card. It can be either "horizontal" or "vertical".
+       */
+      buttonLayout: "horizontal" | "vertical";
+    }
+
+    /**
+     * Interface for a card with a mandatory title.
+     */
+    export interface ITitledCard extends ICardBase {
+      /**
+       * The title of the card. It must be a string with a maximum length of 50 characters.
+       */
+      title: string & tags.MaxLength<50>;
+    }
+
+    /**
+     * Interface for a card with a mandatory description.
+     */
+    export interface IDescripedCard extends ICardBase {
+      /**
+       * The description of the card. It must be a string with a maximum length of 400 characters.
+       */
+      description: string & tags.MaxLength<400>;
+    }
   }
 }
